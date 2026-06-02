e51d12c637d79eec8fd6f6c12b1acb5b919dbef3
Fikaru <fikaru@example.com>
2026-06-02 21:22:03 +0700

    Use custom generated Prisma Client path to resolve Vercel serverless function bundling issue

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@
  */
 
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('./prisma/generated/client');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
